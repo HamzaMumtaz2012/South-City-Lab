0916d978265fc5711b2a7a24055bd083950bde09
Hamza Mumtaz <hamzamumtaz6981@gmail.com>
2026-09-02 15:38:47 +0500

    final project

---
+++ Components/ServiceCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServiceCard = (props) => {
+  return (
+    <div className='flex flex-col items-center'>
+      <img className='h-30' src="/flask.png" alt="" />
+      <p className='text-2xl text-red-600 font-sans font-medium '>{props.text}</p>
+    </div>
+  )
+}
+
+export default ServiceCard
